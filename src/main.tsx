ec770d585be45dc41d87db5ccb6c2143da212ccc
Ferdiyanzah <mferdiyanzah@gmail.com>
2023-02-04 20:11:10 +0700

    add redux, router, react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './services/reducers/store';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
