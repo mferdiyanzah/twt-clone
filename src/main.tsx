d6ace6d269046f0c1792aaac7869423effd82dfb
Ferdiyanzah <mferdiyanzah@gmail.com>
2023-02-05 10:37:13 +0700

    add variable scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { Provider } from 'react-redux';
 import store from './services/reducers/store';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import './assets/scss/_main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
